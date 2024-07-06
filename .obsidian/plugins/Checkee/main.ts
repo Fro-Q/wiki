c6e210b5a4c39b49e5667886d9af7ae6ad4160b4
Fro-Q <qingzhengye@outlook.com>
2024-07-07 07:05:49 +0800

    vault backup: 2024-07-07 07:05:49 num of changes: 81
    Affected files:
    .obsidian/app.json
    .obsidian/appearance.json
    .obsidian/community-plugins.json
    .obsidian/core-plugins-migration.json
    .obsidian/core-plugins.json
    .obsidian/daily-notes.json
    .obsidian/hotkeys.json
    .obsidian/plugins/Checkee/main.ts
    .obsidian/plugins/Checkee/manifest.json
    .obsidian/plugins/attachment-management/data.json
    .obsidian/plugins/attachment-management/main.js
    .obsidian/plugins/attachment-management/manifest.json
    .obsidian/plugins/attachment-management/styles.css
    .obsidian/plugins/auto-card-link/main.js
    .obsidian/plugins/auto-card-link/manifest.json
    .obsidian/plugins/auto-card-link/styles.css
    .obsidian/plugins/calendar/data.json
    .obsidian/plugins/calendar/main.js
    .obsidian/plugins/calendar/manifest.json
    .obsidian/plugins/dataview/main.js
    .obsidian/plugins/dataview/manifest.json
    .obsidian/plugins/easy-typing-obsidian/data.json
    .obsidian/plugins/easy-typing-obsidian/main.js
    .obsidian/plugins/easy-typing-obsidian/manifest.json
    .obsidian/plugins/obsidian-citation-plugin/data.json
    .obsidian/plugins/obsidian-citation-plugin/main.js
    .obsidian/plugins/obsidian-citation-plugin/manifest.json
    .obsidian/plugins/obsidian-citation-plugin/styles.css
    .obsidian/plugins/obsidian-git/data.json
    .obsidian/plugins/obsidian-git/main.js
    .obsidian/plugins/obsidian-git/manifest.json
    .obsidian/plugins/obsidian-git/styles.css
    .obsidian/plugins/obsidian-link-converter/data.json
    .obsidian/plugins/obsidian-link-converter/main.js
    .obsidian/plugins/obsidian-link-converter/manifest.json
    .obsidian/plugins/obsidian-link-converter/styles.css
    .obsidian/plugins/obsidian-linter/data.json
    .obsidian/plugins/obsidian-linter/main.js
    .obsidian/plugins/obsidian-linter/manifest.json
    .obsidian/plugins/obsidian-pandoc-reference-list/data.json
    .obsidian/plugins/obsidian-pandoc-reference-list/main.js
    .obsidian/plugins/obsidian-pandoc-reference-list/manifest.json
    .obsidian/plugins/obsidian-pandoc-reference-list/styles.css
    .obsidian/plugins/obsidian-smarter-md-hotkeys/main.js
    .obsidian/plugins/obsidian-smarter-md-hotkeys/manifest.json
    .obsidian/plugins/obsidian-style-settings/main.js
    .obsidian/plugins/obsidian-style-settings/manifest.json
    .obsidian/plugins/obsidian-text-format/main.js
    .obsidian/plugins/obsidian-text-format/manifest.json
    .obsidian/plugins/obsidian-underline/main.js
    .obsidian/plugins/obsidian-underline/manifest.json
    .obsidian/plugins/obsidian-zotero-desktop-connector/data.json
    .obsidian/plugins/obsidian-zotero-desktop-connector/main.js
    .obsidian/plugins/obsidian-zotero-desktop-connector/manifest.json
    .obsidian/plugins/obsidian-zotero-desktop-connector/pdfannots2json-darwin-arm64
    .obsidian/plugins/obsidian-zotero-desktop-connector/styles.css
    .obsidian/plugins/quickadd/data.json
    .obsidian/plugins/quickadd/main.js
    .obsidian/plugins/quickadd/manifest.json
    .obsidian/plugins/supercharged-links-obsidian/main.js
    .obsidian/plugins/supercharged-links-obsidian/manifest.json
    .obsidian/plugins/supercharged-links-obsidian/styles.css
    .obsidian/plugins/table-editor-obsidian/data.json
    .obsidian/plugins/table-editor-obsidian/main.js
    .obsidian/plugins/table-editor-obsidian/manifest.json
    .obsidian/plugins/table-editor-obsidian/styles.css
    .obsidian/plugins/tabout/data.json
    .obsidian/plugins/tabout/main.js
    .obsidian/plugins/tabout/manifest.json
    .obsidian/plugins/tabout/styles.css
    .obsidian/plugins/templater-obsidian/main.js
    .obsidian/plugins/templater-obsidian/manifest.json
    .obsidian/plugins/url-into-selection/main.js
    .obsidian/plugins/url-into-selection/manifest.json
    .obsidian/plugins/various-complements/main.js
    .obsidian/plugins/various-complements/manifest.json
    .obsidian/plugins/various-complements/styles.css
    .obsidian/snippets/supercharged-links-gen.css
    .obsidian/templates.json
    .obsidian/workspace.json
    .obsidian/zk-prefixer.json

--- .obsidian/plugins/Checkee/main.ts
+++
@@ -1,7 +0,0 @@
-// making a plugin
-// feature: show a dropdown of custom options to fill the checkbox when the user clicks on the checkbox or trigger the hotkey
-
-// Path: main.ts
-
-
-    console.log("loaded");
